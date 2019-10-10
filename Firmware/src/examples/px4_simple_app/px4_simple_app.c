21248ca3e3a13764f3d1bdde3d2273effb8e58ea
wesleyfurnas <wesleyfurnas35@gmail.com>
2019-10-10 08:52:03 -0300

    first algorithim for detection apogeu

--- Firmware/src/examples/px4_simple_app/px4_simple_app.c
+++ Firmware/src/examples/px4_simple_app/px4_simple_app.c
@@ -12,21 +12,22 @@
 #include <uORB/uORB.h>
 #include <uORB/topics/sensor_combined.h>
 #include <uORB/topics/sensor_baro.h>
-#include <uORB/topics/vehicle_attitude.h>
+//#include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/vehicle_air_data.h>
 #include <uORB/topics/tune_control.h>
 
 
 #include <stdbool.h>
 #include <stdio.h>
 
-//#include <debug.h>
+#include <debug.h>
 #include <errno.h>
 
-//#include <nuttx/board.h>
+#include <nuttx/board.h>
 
-//#include <nuttx/sdio.h>
-//#include <nuttx/mmcsd.h>
-//#include <nuttx/analog/adc.h>
+#include <nuttx/sdio.h>
+#include <nuttx/mmcsd.h>
+#include <nuttx/analog/adc.h>
 
 #include <stm32.h>
 #include "board_config.h"
@@ -46,13 +47,14 @@ __EXPORT int px4_simple_app_main(int argc, char *argv[]);
 
 int px4_simple_app_main(int argc, char *argv[])
 {
-	PX4_INFO("Hello Sky!");
-
+	//PX4_INFO("Hello Sky!");
 	/* subscribe to sensor_combined topic */
+	
 	int sensor_sub_fd  = orb_subscribe(ORB_ID(sensor_combined));
+	
 	/*criados por mim*/
 	int sensor_sub_baro = orb_subscribe(ORB_ID(sensor_baro));
-	int veihcule_sub_fd = orb_subscribe(ORB_ID(vehicle_local_position));
+	int veihcule_sub_fd = orb_subscribe(ORB_ID(vehicle_air_data));
 	
 	/* limit the update rate to 5 Hz */
 	orb_set_interval(sensor_sub_fd, 500);
@@ -63,8 +65,8 @@ int px4_simple_app_main(int argc, char *argv[])
 	//struct tune_control_s att;
 	//memset(&att, 0, sizeof(att));
 	//orb_advert_t att_pub = orb_advertise(ORB_ID(tune_control), &att);
-
 	/* one could wait for multiple topics with this technique, just using one here */
+	
 	px4_pollfd_struct_t fds[] = {
 		{ .fd = sensor_sub_fd,   .events = POLLIN },
 		{ .fd = sensor_sub_baro,   .events = POLLIN },
@@ -78,13 +80,14 @@ int px4_simple_app_main(int argc, char *argv[])
 	int error_counter = 0;
 	//int i = 10000;
 	
+	/*configure gpioOutput*/
 	stm32_configgpio(GPIO_GPIO0_OUTPUT);
 	stm32_configgpio(GPIO_GPIO1_OUTPUT);
 	stm32_configgpio(GPIO_GPIO2_OUTPUT);
 	stm32_configgpio(GPIO_GPIO3_OUTPUT);
 	stm32_configgpio(GPIO_GPIO4_OUTPUT);
 	stm32_configgpio(GPIO_GPIO5_OUTPUT);
-	
+	/*set for high ou low gpio*/
 	stm32_gpiowrite(GPIO_GPIO0_OUTPUT,1);
 	stm32_gpiowrite(GPIO_GPIO1_OUTPUT,1);
 	stm32_gpiowrite(GPIO_GPIO2_OUTPUT,1);
@@ -92,15 +95,16 @@ int px4_simple_app_main(int argc, char *argv[])
 	stm32_gpiowrite(GPIO_GPIO3_OUTPUT,1);
 	stm32_gpiowrite(GPIO_GPIO4_OUTPUT,1);
 	stm32_gpiowrite(GPIO_GPIO5_OUTPUT,0);
-	
-	//double baro1 = 0;
-	//double baro2 = 0;
-	//double baro3 = 0;
-	
+	/*buffers determine variation altitude negative or positive*/
+	double a1 = 0;
+	double a2 = 0;
+	double a3 = 0;
+	/*state 0 = idle ; state 1 = running */
+	int state = 0;
 
 	while(true){
-		//baro3 = baro2;
-		//baro2 = baro1;
+		a3 = a2;
+		a2 = a1;
 		/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
 		int poll_ret = px4_poll(fds, 1, 1000);
 
@@ -123,52 +127,52 @@ int px4_simple_app_main(int argc, char *argv[])
 			if (fds[0].revents & POLLIN) {
 				/* obtained data for the first file descriptor */
 				struct sensor_combined_s raw;
+				struct sensor_baro_s rawbaro;
+				// data for altimeter in meters
+				struct vehicle_air_data_s rawVeicule;   
 				/* copy sensors raw data into local buffer */
 				orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
+				orb_copy(ORB_ID(sensor_baro), sensor_sub_baro, &rawbaro);
+				orb_copy(ORB_ID(vehicle_air_data), veihcule_sub_fd, &rawVeicule);
+				
 				PX4_INFO("Accelerometer:\t%8.4f\t%8.4f\t%8.4f",
 					 (double)raw.accelerometer_m_s2[0],
 					 (double)raw.accelerometer_m_s2[1],
 					 (double)raw.accelerometer_m_s2[2]);
+			        PX4_INFO("Barometer:\t%8.4f",
+					 (double)rawbaro.pressure);
+				PX4_INFO("Attitude: \t%8.4f",
+			        	 (double)rawVeicule.baro_alt_meter);
 
 				/* set att and publish this information for other apps
 				 the following does not have any meaning, it's just an example
 				*/
-				/*att.q[0] = raw.accelerometer_m_s2[0];
-				att.q[1] = raw.accelerometer_m_s2[1];
-				att.q[2] = raw.accelerometer_m_s2[2];*/
-				att.volume = 40;
-				att.tune_id= 14;
-				att.frequency = 1;
-				att.duration = 1000;
-                                att.silence = 0;
-                                att.tune_override = 1;
-                                 
 				
-		
 				/* obtained data for the first file descriptor */
-				struct sensor_baro_s rawbaro;
-				struct vehicle_attitude_s rawVeicule;
+				
 				/* copy sensors raw data into local buffer */
-				orb_copy(ORB_ID(sensor_baro), sensor_sub_baro, &rawbaro);
-				PX4_INFO("Barometer:\t%8.4f",
-					 (double)rawbaro.pressure);
-				
-				orb_copy(ORB_ID(vehicle_attitude), veihcule_sub_fd, &rawVeicule);
-				
-			        PX4_INFO("Attitude: %f",
-			        	 (double)rawVeicule.z)
-			
-			 	 // baro1 = rawbaro.pressure;
-			 	  
-				    stm32_gpiowrite(GPIO_GPIO5_OUTPUT,0);
-				    px4_sleep(1);
-				    
-				     PX4_INFO("aqui");
+				   /*update the altitude actual*/
+				   
+			 	    a1 = rawVeicule.baro_alt_meter;
+			 	    /*verify the init of airdrop*/
+			 	    if(a1 > 2.0){
+			 	    	state = 1;
+			 	    }
+			 	    if(state == 1){
+			 	    	if((a1 < a2) && (a2 < a3) ){
+			 	    	   stm32_gpiowrite(GPIO_GPIO5_OUTPUT,1);
+			 	    	   PX4_INFO("Apogeu was : \t%8.4f meters",
+			        	 a3);
+
+			 	    	}
+			 	    }
+				   // stm32_gpiowrite(GPIO_GPIO5_OUTPUT,0);
+				   //px4_sleep(1);
+				   //PX4_INFO("aqui");
 				     
-				       stm32_gpiowrite(GPIO_GPIO5_OUTPUT,1);
-				       px4_sleep(1);
+				   //px4_sleep(1);
 				  // px4_sleep(2);
-				    orb_publish(ORB_ID(tune_control), att_pub, &att);
+				   // orb_publish(ORB_ID(tune_control), att_pub, &att);
 				 /* set att and publish this information for other apps
 				 the following does not have any meaning, it's just an example
 				*/
